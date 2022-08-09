313027c425c94a4a21d2f606f69c09fb490346b3
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-08-09 18:07:41 +0530

    Variable completed

---
+++ src/main/java/ApsheyaAndHaripriya/WebdevelopmentinJava/Variables.java
@@ -0,0 +1,22 @@
+package ApsheyaAndHaripriya.WebdevelopmentinJava;
+
+public class Variables 
+{
+	static String name="Manojkumar";//Static variable
+	
+	public void task()
+	{
+		char class1='A';///local variable
+		System.out.println(class1);
+	}
+	
+	public static void main(String[] args) 
+	{
+		int a=10;//global or instance avriable
+		System.out.println("a value is "+a);
+		Variables obj=new Variables();//Object creation
+		obj.task();
+		System.out.println("my static name is "+name);
+	}
+
+}
