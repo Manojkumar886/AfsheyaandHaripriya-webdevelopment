7db3c04f34ac05d4745439689759346bec45f8a6
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-08-17 17:57:01 +0530

    Operator over

--- src/main/java/ApsheyaAndHaripriya/WebdevelopmentinJava/ScannerObject.java
+++ src/main/java/ApsheyaAndHaripriya/WebdevelopmentinJava/ScannerObject.java
@@ -7,12 +7,12 @@ public class ScannerObject {
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		Scanner scan=new Scanner(System.in);//scanner object
+		Scanner Scan=new Scanner(System.in);//scanner object
 		System.out.println("Enter your name");
-		String name=scan.nextLine();//global variable
+		String name=Scan.nextLine();//global variable
 		System.out.println("my name is :"+name);
 		System.out.println("Enter your age");
-		int age=scan.nextInt();
+		int age=Scan.nextInt();
 		System.out.println("my age is : "+age);
 		
 
