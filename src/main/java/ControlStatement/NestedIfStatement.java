73c9774e8e8a7bb08579fc60478fbed6df6236da
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-08-19 17:36:45 +0530

    80% completed in control statement

---
+++ src/main/java/ControlStatement/NestedIfStatement.java
@@ -0,0 +1,17 @@
+package ControlStatement;
+
+public class NestedIfStatement {
+
+	public static void main(String[] args) {
+		float height=165.8f;
+		float weight=58.9f;
+		if(height==weight)
+		{
+			if(height>weight)
+			{
+				System.out.println("Height is biggest value");
+			}
+		}
+	}
+
+}
