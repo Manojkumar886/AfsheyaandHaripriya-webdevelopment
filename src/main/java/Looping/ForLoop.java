edd735a83e177f67b8991cc6532ab01e3a45919c
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-08-24 17:49:34 +0530

    Looping is Over

---
+++ src/main/java/Looping/ForLoop.java
@@ -0,0 +1,29 @@
+package Looping;
+
+public class ForLoop {
+
+	public static void main(String[] args) 
+	{
+//		for(int i=0;i<=10;i++)
+//		{
+//			System.out.println("Your value is"+i);
+//		}
+		
+//		int a=10;
+//		while(a<=100)
+//		{
+//			System.out.println("Apsheya and haripriya"+a);
+//			a+=10;
+//		}
+		int a=0;
+		do
+		{
+			System.out.println("i am Manoj");
+			a++;
+		}
+		while(a<=-5);
+		
+
+	}
+
+}
